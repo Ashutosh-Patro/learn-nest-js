8ddd31f3389adc541b8c93f73c978f2611906db5
Ashutosh Patro <ashutosh@itobuz.com>
2024-11-14 13:52:56 +0530

    checking commit for song routes

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+  @Post()
+  create() {
+    return 'Create a new song';
+  }
+
+  @Get()
+  findAll() {
+    return 'Find all songs';
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'Fetch song based on ID';
+  }
+
+  @Put(':id')
+  update() {
+    return 'Update song based on ID';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'Delete song based on ID';
+  }
+}
