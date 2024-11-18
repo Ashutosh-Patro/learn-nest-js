6288129ce706b93ae9d33d1ea0aa9b3ab39716f1
Ashutosh Patro <ashutosh@itobuz.com>
2024-11-18 17:38:10 +0530

    learnt all types of providers

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DevConfigService } from './common/providers/DevConfigService';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private devConfigService: DevConfigService,
+    @Inject('CONFIG')
+    private config: { port: string },
+  ) {}
+
   getHello(): string {
-    return 'Hello All!';
+    return `Hello, this is my dev config service ${this.devConfigService.getDBHOST()} PORT => ${this.config.port}`;
   }
 }
