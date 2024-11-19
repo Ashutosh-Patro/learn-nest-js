ff05c92012dbf6d7bcff98d814546d35d9b2262d
Ashutosh Patro <ashutosh@itobuz.com>
2024-11-19 15:04:44 +0530

    created playlists module controller and services foundation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,39 +4,33 @@ import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 import { SongsController } from './songs/songs.controller';
-import { DevConfigService } from './common/providers/DevConfigService';
-
-const devConfig = { port: 3000 };
-const proConfig = { port: 4000 };
+import { PlaylistsModule } from './playlists/playlists.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Song } from './songs/song.entity';
+import { Artist } from './artists/artist.entity';
+import { User } from './users/user.entity';
+import { Playlist } from './playlists/playlist.entity';
 
 @Module({
-  imports: [SongsModule],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: DevConfigService,
-      useClass: DevConfigService,
-    },
-    {
-      provide: 'CONFIG',
-      useFactory: () => {
-        return process.env.NODE_ENV === 'development' ? devConfig : proConfig;
-      },
-    },
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'spotify-clone',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      entities: [Song, Artist, User, Playlist],
+      synchronize: true,
+    }),
+    SongsModule,
+    PlaylistsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    // Method 1 to use LoggerMiddleware for all request types
-    // consumer.apply(LoggerMiddleware).forRoutes('songs');
-
-    // Method 2 to use LoggerMiddleware
-    // consumer
-    //   .apply(LoggerMiddleware)
-    //   .forRoutes({ path: 'songs', method: RequestMethod.POST }); // This just states that the logger middleware will only work when a POST request is called.
-
-    // Method 3 to use LoggerMiddleware for all request types
     consumer.apply(LoggerMiddleware).forRoutes(SongsController);
   }
 }
